ce39d9abb8101e2e16cfa4fe7cec857bf7b5c69f
Salim-T <tizi_salim@yahoo.fr>
2025-06-15 15:52:42 +0200

    add new client

--- server.js
+++ server.js
@@ -7,22 +7,25 @@ console.log('WebSocket server is running on ws://localhost:8080');
 server.on('connection', (socket) => {
     // Handle new client connection
     console.log('New client connected');
-
     // Send a welcome message to the client
     socket.send('Welcome to the WebSocket server!');
 
-
+    server.clients.forEach((client) => {
+        client.send('A new client has joined the server.');
+    });
 
     // Handle incoming messages from the client
     socket.on('message', (message) => {
         console.log(`Received message: ${message}`);
         // Echo the message back to the client
-        socket.send(`You said: ${message}`);
+        server.clients.forEach((client) => {
+            client.send(`Client says: ${message}`);
+        });
+        // socket.send(`You said: ${message}`);
     });
 
     // Handle client disconnection
     socket.on('close', () => {
         console.log('Client disconnected');
     });
-
 });
