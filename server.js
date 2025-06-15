73db285e23b83f2b85427da912f8e41df6620f87
Salim-T <tizi_salim@yahoo.fr>
2025-06-15 16:38:59 +0200

    update server with new client set and verifications, add name to new user before send message, add some css

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const server = new webSocket.Server({ port: 8080 });
 
 console.log('WebSocket server is running on ws://localhost:8080');
 
+const clients = new Set();
+
 server.on('connection', (socket) => {
     // Handle new client connection
     console.log('New client connected');
     // Send a welcome message to the client
     socket.send('Welcome to the WebSocket server!');
 
-    server.clients.forEach((client) => {
+    clients.add(socket);
+
+    clients.forEach((client) => {
+        if (client !== socket && client.readyState === webSocket.OPEN) {
         client.send('A new client has joined the server.');
     });
 
@@ -18,8 +23,10 @@ server.on('connection', (socket) => {
     socket.on('message', (message) => {
         console.log(`Received message: ${message}`);
         // Echo the message back to the client
-        server.clients.forEach((client) => {
-            client.send(`Client says: ${message}`);
+        clients.forEach((client) => {
+            if (client.readyState === webSocket.OPEN) {
+                client.send(`${message}`);
+            }
         });
         // socket.send(`You said: ${message}`);
     });
